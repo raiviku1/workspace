9b3ff23fe282049e108515a36134be1dc72385eb
raiviku1 <raiviku1@gmail.com>
2019-10-29 06:10:38 +0000

    20191029

---
+++ ccmc-03/building.php
@@ -0,0 +1,43 @@
+<?php
+class building{
+    //properties
+     private $name;
+     private $height;
+     
+     
+     function getname($name){
+         $this ->name =$name;
+         $this ->height =$height;
+         
+     }
+     function setname($name){
+          $this ->name = $name;
+     }
+     function getname(){
+         return $this ->name;
+     }
+       function setheight($height){
+          $this ->height = $height;
+         
+     }
+     
+     function getheight(){
+         return $this ->height;
+     }
+     function getprofile(){
+         return $this ->height;
+         
+     }
+  function getprofile(){
+       $profile = "{$this- >name}の高さは{$this- >height)高さmです."
+      return $profile;
+    
+    }
+
+    
+    
+}
+
+?>
+
+
